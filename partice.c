19c5f18e25cd09342f907e58b525ad7a1f30143a
zeeshanthedev590 <62704414+zeeshanthedev590@users.noreply.github.com>
2020-07-28 11:27:53 -0700

    Rectangle Area Checker In C
    Check It Now

---
+++ partice.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+    printf("Enter The Length Of The Rectangle :");
+    scanf("%d", &a);
+    printf("Enter The Breath Of The Rectangle : ");
+    scanf("%d", &b);
+    c = a * b;
+    printf("The Area of The Rectangle Will Be %d", c);
+    return 0;
+}
+
+
